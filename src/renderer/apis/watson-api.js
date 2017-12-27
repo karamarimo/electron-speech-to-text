e454b0a4f5d79aacccbf43431a57f83e3c0d3f70
karamarimo <kubotaryou.ryoukubota@gmail.com>
2017-12-27 20:53:03 +0900

    implement mic recognition (not working well)

---
+++ src/renderer/apis/watson-api.js
@@ -0,0 +1,40 @@
+const watson = require('watson-developer-cloud')
+const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1')
+const credentails = require('../../../credentials.json')
+const recognizeMicrophone = require('watson-speech/speech-to-text/recognize-microphone')
+
+const service = new SpeechToTextV1(credentails)
+const authService = new watson.AuthorizationV1(credentails)
+
+export function recognizeStream (stream, params = {}) {
+  const defaultParams = {
+    model: 'en-US_BroadbandModel',
+    'content_type': 'audio/wav',
+    'interim_results': true,
+    'max_alternatives': 3,
+    'word_confidence': false,
+    speaker_labels: false,
+    timestamps: false,
+    objectMode: true
+  }
+
+  const mergedParams = Object.assign(defaultParams, params)
+  const recognizeStream = service.createRecognizeStream(mergedParams)
+  console.log(mergedParams)
+  stream.pipe(recognizeStream)
+  return recognizeStream
+}
+
+export function recognizeMic (outputElement) {
+  return new Promise((resolve, reject) => {
+    authService.getToken({
+      url: watson.SpeechToTextV1.URL
+    }, (err, token) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(recognizeMicrophone({ token, outputElement }))
+    })
+  })
+}
--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,23 +1,129 @@
 <template>
-  <div id="wrapper">
-    <!-- <img id="logo" src="~@/assets/logo.png" alt="electron-vue"> -->
-    <main>
-      Hello world!
-    </main>
-  </div>
+  <section class="section">
+    <div class="container">
+      <div class="container is-fullhd level">
+        <div class="level-left">
+          <a @click="toggleRecord" class="button is-primary level-item">
+            {{ recording ? 'Stop' : 'Start'}}
+          </a>
+        </div>
+        <div class="level-right">
+          <a @click="clear" class="button is-danger level-item">Clear</a>
+        </div>
+      </div>
+      <div class="tabs is-boxed">
+        <ul>
+          <li v-for="(file, i) in files" :key="i" 
+              :class="{'is-active': activeFile === i}">
+            <a @click="selectFile(i)">{{file.name}}</a>
+          </li>
+          <li><a @click="addFile">
+            <span class="icon has-text-primary">
+              <i class="fa fa-plus"></i>
+            </span>
+          </a></li>
+        </ul>
+      </div>
+      <div class="editor">
+        <textarea v-model="files[activeFile].text"
+          class="textarea" placeholder="transcription will be shown here"></textarea>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-  export default {
-    components: {
+import { recognizeStream } from '@/apis/watson-api'
+import * as mic from 'mic'
+import * as wav from 'wav'
 
-    },
-    methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
+export default {
+  data () {
+    return {
+      files: [{name: 'New File', text: ''}],
+      recording: false,
+      activeFile: 0,
+      micInstance: null,
+      recognizeStream: null,
+      micConfig: {
+        rate: '16000',
+        channels: '1',
+        fileType: 'raw'
       }
     }
+  },
+  methods: {
+    open (link) {
+      this.$electron.shell.openExternal(link)
+    },
+    clear () {
+      this.files[this.activeFile].text = ''
+    },
+    toggleRecord () {
+      if (this.recording) {
+        this.stopRecording()
+      } else {
+        this.startRecording()
+      }
+    },
+    startRecording () {
+      this.recording = true
+
+      this.micInstance = mic(this.micConfig)
+      const wavStream = new wav.Writer({
+        sampleRate: parseInt(this.micConfig.rate),
+        channels: parseInt(this.micConfig.channels)
+      })
+      // const recStream = recognizeStream(stream)
+      const recStream = recognizeStream(wavStream)
+      const stream = this.micInstance.getAudioStream()
+      stream.pipe(wavStream)
+      // wavStream.on('data', (data) => console.log(data))
+
+      recStream.on('data', (event) => this.recCallback(null, 'data', event))
+      recStream.on('error', (event) => this.recCallback(event))
+      recStream.on('close', (event) => this.recCallback(null, 'close', event))
+      // recStream.pipe(process.stdout)
+      this.micInstance.start()
+    },
+    recCallback (err, type, event) {
+      if (err) {
+        console.error(err)
+        if (this.recording) this.stopRecording()
+        return
+      }
+      if (type === 'close') {
+        console.log('connection closed')
+        if (this.recording) this.stopRecording()
+        return
+      }
+      if (event.result_index) {
+        console.log('index: ' + event.result_index)
+        for (let result of event.results || []) {
+          console.log(result.alternatives[0].transcript, result.final)
+        }
+      }
+      if (event.speaker_labels) {
+        console.log(event.speaker_labels)
+      }
+    },
+    stopRecording () {
+      this.micInstance.stop()
+      this.recording = false
+    },
+    selectFile (index) {
+      if (this.recording) return
+      this.activeFile = index
+    },
+    addFile () {
+      if (this.recording) return
+      this.files.push({
+        name: 'New File',
+        text: ''
+      })
+    }
   }
+}
 </script>
 
 <style>
